5b8f3c76f73881f0373b3da6d2427dda2006d299
midlajc <mail.midlajc@gmail.com>
2021-10-21 16:29:56 +0530

    removed index.html

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', rootRouter);
 
-app.use(express.static(path.join(__dirname, 'public')));
 module.exports = app;

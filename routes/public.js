8b13d3af377c6f453bc52cd7ed0df27dc2e3a2d0
midlajc <mail.midlajc@gmail.com>
2022-05-07 07:01:15 +0530

    gender public api

--- routes/public.js
+++ routes/public.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const publicService = require("../service/publicService");
+const dataService = require("../service/dataService");
 
 router.route("/sponsor-registration").post((req, res) => {
   publicService
     .sponsorRegistration(req.body)
     .then((response) => {
       res.status(200).json(response);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+router.route("/gender").get((req, res) => {
+  dataService
+    .genderList()
+    .then((responses) => {
+      res.status(200).json(responses);
     })
     .catch((err) => {
       res.status(500).json(err);
